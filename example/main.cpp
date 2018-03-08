edae95b327439bce044f1df5044c55a2a3c66425
Andy Maloney <asmaloney@gmail.com>
2018-03-08 13:24:36 -0500

    {example} Add map so we can display the QImage::Format enum as a string

--- example/main.cpp
+++ example/main.cpp
@@ -12,11 +12,41 @@
 #include <QFileInfo>
 #include <QImageReader>
 #include <QImageWriter>
+#include <QMap>
 
 #include "asmOpenCV.h"
 
+
 static void  sImageBlur( const QString &inInputFile, const QString &inOutputFile );
 
+// Simple map of enum value to string since C++ STILL doesn't provide this...
+static QMap<QImage::Format, QString>   sImageFormatEnumMap = {
+   { QImage::Format::Format_Invalid, "Format_Invalid" },
+   { QImage::Format::Format_Mono, "Format_Mono" },
+   { QImage::Format::Format_MonoLSB, "Format_MonoLSB" },
+   { QImage::Format::Format_Indexed8, "Format_Indexed8" },
+   { QImage::Format::Format_RGB32, "Format_RGB32" },
+   { QImage::Format::Format_ARGB32, "Format_ARGB32" },
+   { QImage::Format::Format_ARGB32_Premultiplied, "Format_ARGB32_Premultiplied" },
+   { QImage::Format::Format_RGB16, "Format_RGB16" },
+   { QImage::Format::Format_ARGB8565_Premultiplied, "Format_ARGB8565_Premultiplied" },
+   { QImage::Format::Format_RGB666, "Format_RGB666" },
+   { QImage::Format::Format_ARGB6666_Premultiplied, "Format_ARGB6666_Premultiplied" },
+   { QImage::Format::Format_RGB555, "Format_RGB555" },
+   { QImage::Format::Format_ARGB8555_Premultiplied, "Format_ARGB8555_Premultiplied" },
+   { QImage::Format::Format_RGB888, "Format_RGB888" },
+   { QImage::Format::Format_RGB444, "Format_RGB444" },
+   { QImage::Format::Format_ARGB4444_Premultiplied, "Format_ARGB4444_Premultiplied" },
+   { QImage::Format::Format_RGBX8888, "Format_RGBX8888" },
+   { QImage::Format::Format_RGBA8888, "Format_RGBA8888" },
+   { QImage::Format::Format_RGBA8888_Premultiplied, "Format_RGBA8888_Premultiplied" },
+   { QImage::Format::Format_BGR30, "Format_BGR30" },
+   { QImage::Format::Format_A2BGR30_Premultiplied, "Format_A2BGR30_Premultiplied" },
+   { QImage::Format::Format_RGB30, "Format_RGB30" },
+   { QImage::Format::Format_A2RGB30_Premultiplied, "Format_A2RGB30_Premultiplied" },
+   { QImage::Format::Format_Alpha8, "Format_Alpha8" },
+   { QImage::Format::Format_Grayscale8, "Format_Grayscale8" }
+};
 
 int  main( int argc, char **argv )
 {
@@ -63,7 +93,7 @@ void  sImageBlur( const QString &inInputFile, const QString &inOutputFile )
       return;
    }
 
-   qInfo() << "Input image format:" << image.format();
+   qInfo().noquote() << "Input image format:" << sImageFormatEnumMap[image.format()];
 
    // Convert QImage to a cvMat
    cv::Mat  cvMat = ASM::QImageToCvMat( image, false );
@@ -73,6 +103,7 @@ void  sImageBlur( const QString &inInputFile, const QString &inOutputFile )
 
    // Convert back to QImage so we can write it out
    QImage   blurred = ASM::cvMatToQImage( cvMat );
+   qInfo().noquote() << "blurred image format:" << sImageFormatEnumMap[blurred.format()];
 
    QImageWriter   writer( inOutputFile, "png" );
 
